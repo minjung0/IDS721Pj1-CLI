15058b572920d8c2d6757174590a04dec83843d3
Minjung Lee <90014065+minjung0@users.noreply.github.com>
2023-02-08 06:32:05 +0000

    add Fall 2023, Spring 2024

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,51 @@
 use std::io;
 
 fn main() {
-    println!("Input your credits for Spring 2023");
+    println!("Which semester's tuition: 1. Spring 2023, 2. Fall 2023 or Spring 2024");
+
+    let mut semester = String::new();
+
+    io::stdin()
+        .read_line(&mut semester)
+        .expect("Failed to read line.");
+
+    let semester: u32 = semester.trim().parse().unwrap_or(1);
+
+    match semester {
+        1 => println!("You choose Spring 2023."),
+        2 => println!("You choose Fall 2023 or Spring 2024."),
+        _ => panic!("Please choose 1 or 2."),
+    };
+
+    println!("Input your credits for the semester.");
 
     let mut credit = String::new();
 
     io::stdin()
         .read_line(&mut credit)
-        .expect("Failed to read line");
-    
+        .expect("Failed to read line.");
+
     let credit: f32 = credit.trim().parse().unwrap_or(0.0);
 
-    // Full time: 30,110
-    // Part time: 3,495 per credit
+    // (2022-2023) Full time: 30,110
+    // (2023-2024)            31,310
+    // (2022-2023) Part time: 3,495 per credit
+    // (2023-2024)            3,635
     if credit >= 9.0 {
         println!("You are a full-time student.");
-        println!("Your tuition is 30,110 dollars ")
-    }
-    else {
-        let pt= 3495.0 * credit;
+        if semester == 1 {
+            println!("Your tuition is 30,110 dollars.");
+        } else {
+            println!("Your tuition is 31,310 dollars.");
+        }
+    } else {
+        let mut pt = 0.0;
         println!("You are a part-time student.");
-        println!("Your tuition is {} dollars", pt)
+        if semester == 1 {
+            pt = 3490.0 * credit;
+        } else {
+            pt = 3635.0 * credit;
+        }
+        println!("Your tuition is {} dollars.", pt);
     }
 }
